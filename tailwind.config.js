db0da5894b847e56f5e60d7ad7d4aa8d302c09ff
JongMany <blackberry1114@naver.com>
2024-11-29 19:54:28 +0900

    tailwind tokens설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,71 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        text: "18px",
+        bar: "166px",
+      },
+      width: {
+        bar: "328px",
+      },
+    },
+    borderRadius: {
+      default: "5px",
+      caption: "9px",
+      "tag-button": "16px",
+      "chat-text-field": "20px",
+    },
+    // Font
+    fontFamily: {
+      body: ["Noto Sans KR"],
+    },
+    colors: {
+      primary: "#1d2745",
+      secondary: "#1de5d4",
+      tertiary: "#f52c50",
+      white: "#ffffff",
+      mono100: "#f1f1f1",
+      mono200: "#bebebe",
+      mono300: "d6d7e9",
+      error: "#d01e1e",
+      social: "#395997",
+    },
+    fontSize: {
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
   },
   plugins: [],
 };
