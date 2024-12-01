d5cc9eff6815d1671bf92d4513976d0b3fe10951
JongMany <blackberry1114@naver.com>
2024-12-01 13:51:53 +0900

    DefaultTextField 컴포넌트 스타일 추가

---
+++ src/components/DefaultTextField.tsx
@@ -0,0 +1,54 @@
+import IconButton from "./IconButton";
+import ErrorMessage from "./ErrorMessage";
+import { useState } from "react";
+
+interface IDefaultTextFieldProps {
+  errorMessage: string;
+  iconPath: string;
+  iconAlt: string;
+  onIconClick: React.MouseEventHandler<HTMLButtonElement>;
+  placeholder: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
+  isError: boolean;
+}
+
+export default function DefaultTextField({
+  errorMessage,
+  iconAlt,
+  iconPath,
+  onIconClick,
+  placeholder,
+  onChange,
+  value,
+  isError,
+}: IDefaultTextFieldProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const borderColor = isFocused
+    ? "border-secondary"
+    : !value
+    ? "border-mono300"
+    : "border-primary";
+
+  return (
+    <div>
+      <div
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className={`text-primary border-b ${borderColor}`}
+      >
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+        {value.length > 0 && (
+          <IconButton onClick={onIconClick} alt={iconAlt} iconPath={iconPath} />
+        )}
+      </div>
+      {isError && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </div>
+  );
+}
---
+++ src/stories/DefaultTextField.stories.ts
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import DefaultTextField from "../components/DefaultTextField";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "TextFields/DefaultTextField",
+  component: DefaultTextField,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    iconAlt: {
+      control: "text",
+      description: "이미지의 alt 속성",
+      defaultValue: "icon",
+    },
+    iconPath: {
+      control: "text",
+      description: "이미지의 경로",
+      defaultValue: "",
+    },
+    placeholder: {
+      control: "text",
+      description: "텍스트 필드의 Placeholder",
+      defaultValue: "텍스트를 입력해주세요",
+    },
+    value: {
+      control: "text",
+      description: "텍스트 필드의 값",
+      defaultValue: "",
+    },
+    isError: {
+      control: "boolean",
+      description: "텍스트 필드의 에러 여부",
+      defaultValue: false,
+    },
+    errorMessage: {
+      control: "text",
+      description: "텍스트 필드의 에러 메세지",
+      defaultValue: "",
+    },
+    onChange: {
+      action: "changed",
+      description: "텍스트 필드의 값 변경 이벤트 핸들러 함수",
+    },
+    onIconClick: { action: "clicked", description: "버튼 클릭 이벤트" },
+  },
+  args: { onIconClick: fn(), onChange: fn() },
+} satisfies Meta<typeof DefaultTextField>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// import deleteIcon from "../assets/ic-delete-dark.svg";
+export const Default: Story = {
+  args: {
+    iconAlt: "icon",
+    // Public
+    // iconPath: "/icons/ic-delete-dark.svg",
+    // src/assets
+    // iconPath: deleteIcon,
+    // Cloud
+    iconPath: "https://kr.object.ncloudstorage.com/icons/ic-delete-dark.svg",
+    placeholder: "텍스트를 입력해주세요",
+    value: "",
+    errorMessage: "텍스트를 확인해주세요",
+    isError: false,
+  },
+};
