aac854c7ca6fe11770cf415aed1fc65f318accfd
JongMany <blackberry1114@naver.com>
2024-12-04 08:08:54 +0900

    vite 빌드 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import { viteStaticCopy } from "vite-plugin-static-copy";
@@ -12,4 +13,23 @@ export default defineConfig({
       targets: [{ src: "src/index.css", dest: "" }],
     }),
   ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      fileName: (format) => `index.${format}.js`,
+      name: "devlee-design-system",
+    },
+    rollupOptions: {
+      // 외부 라이브러리니 빌드시 추가 X
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 });
