783786a86761031fdba5be6c1b4117d6e1b72882
JongMany <blackberry1114@naver.com>
2024-12-04 07:58:19 +0900

    빌드시 dts, tailwind css가 적용되도록 패키지 설치 및 vite 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts(),
+    viteStaticCopy({
+      targets: [{ src: "src/index.css", dest: "" }],
+    }),
+  ],
+});
